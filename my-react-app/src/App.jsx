c40d4554614b73f27f5b1229743e86987db853f0
arifahnbm <nura60583@gmail.com>
2025-02-01 20:27:12 +0700

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center bg-yellow-300 items-center min-h-screen">
+      <div>Annyeonghaseo Sesang</div>
+    </div>
+  )
+}
+
+export default App
