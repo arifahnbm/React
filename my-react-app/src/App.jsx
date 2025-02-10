32c5b8c3f0124dbfb104f1bb1ba32fa305f87205
arifahnbm <nura60583@gmail.com>
2025-02-10 15:12:53 +0700

    componen dan property

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,22 @@
+import React from "react"
+
+const Button = (props) => {
+  const {textcolor, children} = props;
+  return (
+    <button className={`h-10 px-6 font-semibold rounded-md ${props.color} ${textcolor}`} type="submit">
+        {children}
+    </button>
+  )
+}
+
 function App() {
   return (
     <div className="flex justify-center bg-yellow-300 items-center min-h-screen">
-      <div>Annyeonghaseo Sesang</div>
+      <div className="flex gap-x-3">
+        <Button color="bg-black" textcolor="text-white">Buy now</Button>
+        <Button color="bg-white" textcolor="text-black">Login</Button>
+        <Button color="bg-red-500" textcolor="text-white">Register</Button>
+      </div>
     </div>
   )
 }
